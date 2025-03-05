7a1021995a2fbda91b85995534f349d6b257a5ed
Francisco Juan <juansoussev@gmail.com>
2025-03-05 16:59:33 -0300

    Class 05: Data types

---
+++ fundamentals/Class05IntDoubleFloatTypeOf.swift
@@ -0,0 +1,15 @@
+// Class 05: Data Types, like Int, Double, Float, and TypeOf
+
+import Foundation
+
+var a: Int = 6
+var b: Double = 6.1
+var c: Float = 6.1
+
+/*
+  the primitives data types follows the same structure independent of the language.
+  so:
+  Int -> its about a integer value, like 100, 1, -95, 4
+  Double and Float are very simmilar, both storage numbers breakeds, the main difference is that
+  Double is amazing most precise, what means that its size is bigger than Float
+*/
