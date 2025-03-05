53ca4a7b06606365df68133e02cbb1f52a00e0df
Francisco Juan <juansoussev@gmail.com>
2025-03-04 21:58:02 -0300

    first commit

---
+++ fundamentals/Class01VariablesAndConstants.swift
@@ -0,0 +1,9 @@
+// Var is a variable option, what means its value can change
+
+var greeting = "Hello World!"
+
+print(greeting)
+
+// Let is imutable, it means its value cannot change
+let constant = "Valor Constante"
+print(constant)
