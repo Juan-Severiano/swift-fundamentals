ff41ca6edec715cfe8dce1ada3f96ce8648a01cd
Francisco Juan <juansoussev@gmail.com>
2025-03-04 22:29:55 -0300

    class02: String Arrays and For Looping

---
+++ fundamentals/Class02StringArraysAndFor.swift
@@ -0,0 +1,7 @@
+// The array contains the same pattern like the other languages
+let listOfPeople = ["Eve", "Mark", "Janice"]
+
+// The for structure follow a different structure, maybe mescling python with javascript
+for name: String in listOfPeople {
+  print("Hi \(name), how are u?")
+}
